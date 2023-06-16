a9f7d01f87435f7b5e9a3c8bd7730e9b2ae98e9f
threepolesoft <threepolesoft@gmail.com>
2023-06-16 23:46:30 +0600

    Add project files.

---
+++ WindowsFromAppZipMaker/Form1.Designer.cs
@@ -0,0 +1,128 @@
+﻿namespace WindowsFromAppZipMaker
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnFolderOne = new System.Windows.Forms.Button();
+            this.BtnFolderTwo = new System.Windows.Forms.Button();
+            this.btnCreateToZip = new System.Windows.Forms.Button();
+            this.lbFolderOne = new System.Windows.Forms.Label();
+            this.lbFolderTwo = new System.Windows.Forms.Label();
+            this.lbStatus = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnFolderOne
+            // 
+            this.btnFolderOne.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnFolderOne.Location = new System.Drawing.Point(87, 71);
+            this.btnFolderOne.Name = "btnFolderOne";
+            this.btnFolderOne.Size = new System.Drawing.Size(97, 38);
+            this.btnFolderOne.TabIndex = 0;
+            this.btnFolderOne.Text = "Browse Folder 1";
+            this.btnFolderOne.UseVisualStyleBackColor = true;
+            this.btnFolderOne.Click += new System.EventHandler(this.btnFolderOne_Click);
+            // 
+            // BtnFolderTwo
+            // 
+            this.BtnFolderTwo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.BtnFolderTwo.Location = new System.Drawing.Point(87, 160);
+            this.BtnFolderTwo.Name = "BtnFolderTwo";
+            this.BtnFolderTwo.Size = new System.Drawing.Size(97, 38);
+            this.BtnFolderTwo.TabIndex = 1;
+            this.BtnFolderTwo.Text = "Browse Folder 2";
+            this.BtnFolderTwo.UseVisualStyleBackColor = true;
+            this.BtnFolderTwo.Click += new System.EventHandler(this.BtnFolderTwo_Click);
+            // 
+            // btnCreateToZip
+            // 
+            this.btnCreateToZip.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.btnCreateToZip.Location = new System.Drawing.Point(367, 160);
+            this.btnCreateToZip.Name = "btnCreateToZip";
+            this.btnCreateToZip.Size = new System.Drawing.Size(97, 38);
+            this.btnCreateToZip.TabIndex = 2;
+            this.btnCreateToZip.Text = "Create to Zip";
+            this.btnCreateToZip.UseVisualStyleBackColor = true;
+            this.btnCreateToZip.Click += new System.EventHandler(this.btnCreateToZip_Click);
+            // 
+            // lbFolderOne
+            // 
+            this.lbFolderOne.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbFolderOne.AutoSize = true;
+            this.lbFolderOne.Location = new System.Drawing.Point(87, 52);
+            this.lbFolderOne.Name = "lbFolderOne";
+            this.lbFolderOne.Size = new System.Drawing.Size(0, 13);
+            this.lbFolderOne.TabIndex = 6;
+            // 
+            // lbFolderTwo
+            // 
+            this.lbFolderTwo.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbFolderTwo.AutoSize = true;
+            this.lbFolderTwo.Location = new System.Drawing.Point(87, 141);
+            this.lbFolderTwo.Name = "lbFolderTwo";
+            this.lbFolderTwo.Size = new System.Drawing.Size(0, 13);
+            this.lbFolderTwo.TabIndex = 7;
+            // 
+            // lbStatus
+            // 
+            this.lbStatus.Anchor = System.Windows.Forms.AnchorStyles.None;
+            this.lbStatus.AutoSize = true;
+            this.lbStatus.Location = new System.Drawing.Point(367, 200);
+            this.lbStatus.Name = "lbStatus";
+            this.lbStatus.Size = new System.Drawing.Size(0, 13);
+            this.lbStatus.TabIndex = 8;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(591, 323);
+            this.Controls.Add(this.lbStatus);
+            this.Controls.Add(this.lbFolderTwo);
+            this.Controls.Add(this.lbFolderOne);
+            this.Controls.Add(this.btnCreateToZip);
+            this.Controls.Add(this.BtnFolderTwo);
+            this.Controls.Add(this.btnFolderOne);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Zip Maker";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnFolderOne;
+        private System.Windows.Forms.Button BtnFolderTwo;
+        private System.Windows.Forms.Button btnCreateToZip;
+        private System.Windows.Forms.Label lbFolderOne;
+        private System.Windows.Forms.Label lbFolderTwo;
+        private System.Windows.Forms.Label lbStatus;
+    }
+}
+
---
+++ WindowsFromAppZipMaker/Form1.cs
@@ -0,0 +1,163 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Threading;
+using System.IO.Compression;
+using Microsoft.VisualBasic;
+
+namespace WindowsFromAppZipMaker
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public string first_folder_name = "";
+
+        private void btnFolderOne_Click(object sender, EventArgs e)
+        {
+
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+
+            if (fbd.ShowDialog()==DialogResult.OK)
+            {
+                lbFolderOne.Text = fbd.SelectedPath;
+
+                first_folder_name=Path.GetFileName(fbd.SelectedPath);
+            }
+        }
+
+        public string second_folder_name = "";
+       
+
+        private void BtnFolderTwo_Click(object sender, EventArgs e)
+        {
+            FolderBrowserDialog fbd = new FolderBrowserDialog();
+
+            if (fbd.ShowDialog() == DialogResult.OK)
+            {
+                lbFolderTwo.Text = fbd.SelectedPath;
+
+                second_folder_name= Path.GetFileName(fbd.SelectedPath);
+            }
+        }
+
+        private void btnCreateToZip_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                if (string.IsNullOrEmpty(lbFolderOne.Text))
+                {
+                    MessageBox.Show("Message", "Select first folder and try again");
+                    return;
+                }
+
+                if (string.IsNullOrEmpty(lbFolderTwo.Text))
+                {
+                    MessageBox.Show("Message", "Select first folder and try again");
+                    return;
+                }
+
+                lbStatus.Text = "Preparing...";
+                lbStatus.Update();
+
+
+                string folder=Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData);
+
+                string sff = Path.Combine(folder, "ZipTemp"+"\\"+"temp");
+
+                if (new DirectoryInfo(sff).Exists)
+                {
+                    Directory.Delete(sff, true);
+                }
+
+                Directory.CreateDirectory(sff);
+
+                copy(new DirectoryInfo(lbFolderOne.Text), new DirectoryInfo(sff + "\\" + first_folder_name));
+                copy(new DirectoryInfo(lbFolderTwo.Text), new DirectoryInfo(sff + "\\" + second_folder_name));
+
+
+                if (new FileInfo(lbFolderOne.Text + ".zip").Exists)
+                {
+                    if (MessageBox.Show("Zip output already exixts, want to replace it.", "Message", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                    {
+                        File.Delete(lbFolderOne.Text + ".zip");
+                        lbStatus.Text = "Replaceing...";
+                        lbStatus.Update();
+                        ZipFile.CreateFromDirectory(sff, lbFolderOne.Text + ".zip");
+
+                        Directory.Delete(sff, true);
+
+                        lbStatus.Text = "Finish";
+                        lbStatus.Update();
+                    }
+                    else
+                    {
+                        Directory.Delete(sff, true);
+                        lbStatus.Text = "Cancel";
+                        lbStatus.Update();
+                    }
+                }
+                else
+                {
+                    ZipFile.CreateFromDirectory(sff, lbFolderOne.Text + ".zip");
+                    Directory.Delete(sff, true);
+                    lbStatus.Text = "Finish";
+                    lbStatus.Update();
+                }
+
+                
+
+                //using (FileStream fileStream=new FileStream(lbFolderOne.Text+".zip", FileMode.Open))
+                //{
+                //    using (ZipArchive zipArchive=new ZipArchive(fileStream, ZipArchiveMode.Update))
+                //    {
+                //        ZipArchiveEntry zip;
+                //        DirectoryInfo directoryInfo = new DirectoryInfo(lbFolderTwo.Text);
+
+                //        FileInfo[] files = directoryInfo.GetFiles();
+
+                //        foreach (var item in files)
+                //        {
+                //            zip = zipArchive.CreateEntryFromFile(lbFolderTwo.Text + "\\" + item.Name, second_folder_name + "/" + item.Name);
+                //        }
+                //    }
+                //}
+            }
+            catch (Exception ex)
+            {
+                lbStatus.Text = ex.Message;
+            }
+        }
+
+        public void copy(DirectoryInfo source, DirectoryInfo target)
+        {
+            Directory.CreateDirectory(target.FullName);
+
+
+            foreach (var item in source.GetFiles())
+            {
+                item.CopyTo(Path.Combine(target.FullName, item.Name), true);
+            }
+
+            foreach (var item in source.GetDirectories())
+            {
+                DirectoryInfo directoryInfo = target.CreateSubdirectory(item.Name);
+
+                copy(item, directoryInfo);
+            }
+        }
+    }
+}
